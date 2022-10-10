cc8c92bd709b2304e837c623fe3811fe148d9f93
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-10-11 01:39:38 +0700

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
   searchYelp(term, location, sortBy) {
     Yelp.search(term, location, sortBy).then(
       businesses => {
+        this.setState({ businesses: businesses })
 
       }
     )
